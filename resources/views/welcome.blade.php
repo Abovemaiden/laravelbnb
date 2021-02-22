63875e2b26bb54a14e72eaae245e5dfe15ee7b52
Antonio Aradaza <jungrin28@gmail.com>
2021-02-22 22:15:11 +0800

    add home router

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,21 +5,17 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>LaravelBnb</title>
+  <title>Laravel Bnb</title>
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-  <style>
-    body {
-      font-family: 'Nunito';
-    }
-  </style>
-  <script src="{{ asset('js/app.js') }}"></script>
+  <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body>
-  <div id="app"></div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </body>
 
 </html>
